83ffe848d21bfbe96f1c642a2a3ca0cba16f6240
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-04-13 22:09:56 +0200

    New

--- old.py
+++ old.py
@@ -4,34 +4,27 @@ from random import randint
 from bs4 import BeautifulSoup, SoupStrainer
 
 url = "https://it.wikipedia.org"
-rand_url = "http://en.wikipedia.org/wiki/Special:Random"
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 def condition(s):
     return not ("Lingua" in s or "Lingue" in s or "Linguistica" in s or "Gnome" in s or "Aiuto" in s or "Categoria" in s)
 
-def attempt(limit, query="", i=0):
-    if i == 0:
-        r = requests.get(rand_url, headers=headers)
-    else:
-        r = requests.get(url + query, headers=headers)
-
+def attempt(query, limit, i=0):
+    r = requests.get(url + query, headers=headers)
     soup = BeautifulSoup(r.content, "html.parser", parse_only=SoupStrainer('p'))
 
     for link in soup.find_all("a"):
-        print(link)
         new_query = link.get("href")
         if new_query[0:6] == "/wiki/" and condition(new_query):
             break
 
     print(i, new_query)
     i += 1
-    print("--------")
 
-    if i < limit and new_query != "/wiki/Philosophy":
+    if i < limit and new_query != "/wiki/Filosofia":
         time.sleep(1)
-        return attempt(limit, new_query, i)
+        return attempt(new_query, limit, i)
 
     elif i >= limit:
         print("NOT FOUND")
@@ -41,4 +34,29 @@ def attempt(limit, query="", i=0):
         print("FOUND AFTER", i, "ATTEMPTS")
         return 1
 
-attempt(5)
+def get_query(theme):
+    s = requests.Session()
+
+    url = "https://en.wikipedia.org/w/api.php"
+
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "allpages",
+        "apfrom": theme,
+        # "aplimit": 100,
+    }
+
+    r = s.get(url=url, params=params)
+    data = r.json()
+
+    pages = data["query"]["allpages"]
+
+    return pages[randint(0, len(pages))]["title"].replace("!", "")
+
+theme = "scienza"
+for i in range(10):
+    query = "/wiki/" + get_query(theme)
+    print(query)
+
+    attempt(query, 15)
