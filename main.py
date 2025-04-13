8ef80246f81538a5213a916471d94b90f37c182a
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-04-13 22:36:53 +0200

    new

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ import time
 from random import randint
 from bs4 import BeautifulSoup, SoupStrainer
 
-url = "https://it.wikipedia.org"
+url = "https://en.wikipedia.org"
 rand_url = "http://en.wikipedia.org/wiki/Special:Random"
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 def condition(s):
-    return not ("Lingua" in s or "Lingue" in s or "Linguistica" in s or "Gnome" in s or "Aiuto" in s or "Categoria" in s)
+    return not ("Help" in s)
 
 def attempt(limit, query="", i=0):
+    s = requests.Session()
+    s.cookies["news.lang"] = "en"
+
     if i == 0:
-        r = requests.get(rand_url, headers=headers)
+        r = s.get(rand_url, headers=headers)
     else:
-        r = requests.get(url + query, headers=headers)
+        r = s.get(url + query, headers=headers)
 
     soup = BeautifulSoup(r.content, "html.parser", parse_only=SoupStrainer('p'))
 
     for link in soup.find_all("a"):
-        print(link)
         new_query = link.get("href")
         if new_query[0:6] == "/wiki/" and condition(new_query):
             break
 
     print(i, new_query)
     i += 1
-    print("--------")
 
     if i < limit and new_query != "/wiki/Philosophy":
         time.sleep(1)
@@ -41,4 +42,5 @@ def attempt(limit, query="", i=0):
         print("FOUND AFTER", i, "ATTEMPTS")
         return 1
 
-attempt(5)
+
+    attempt(20)
