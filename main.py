45f05b930ca6c026dfbda6e848f54f5773a236fe
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-04-13 23:15:53 +0200

    New

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import requests
 import time
 from random import randint
 from bs4 import BeautifulSoup, SoupStrainer
+from csv import writer
+
+# number of requests
+n = 2
+
+# number of links to open per page
+x = 15
+
 
 url = "https://en.wikipedia.org"
 rand_url = "http://en.wikipedia.org/wiki/Special:Random"
@@ -9,7 +17,9 @@ rand_url = "http://en.wikipedia.org/wiki/Special:Random"
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 def condition(s):
-    return not ("Help" in s)
+    return not ("Help" in s or "Special" in s)
+
+arr = []
 
 def attempt(limit, query="", i=0):
     s = requests.Session()
@@ -30,24 +40,29 @@ def attempt(limit, query="", i=0):
     print(i, new_query)
     i += 1
 
-    if i < limit and (new_query != "/wiki/Philosophy" or new_query != "/wiki/Philosophical":
+    arr.append(new_query[6:])
+
+    if new_query == "/wiki/Philosophy" or new_query == "/wiki/Philosophical" or i >= limit:
+        return arr
+
+    if i < limit and (new_query != "/wiki/Philosophy" or new_query != "/wiki/Philosophical"):
         time.sleep(1)
         return attempt(limit, new_query, i)
 
-    elif i >= limit:
-        print("NOT FOUND")
-        return 1
-
-    else:
-        print("-------------------")
-        print("FOUND AFTER", i, "ATTEMPTS")
-        return 0
-
-n = 10
-m = 0
+head = [str(i) for i in range(x)]
+data = [head]
 
 for i in range(n):
-    if attempt(20) == 0:
-        m += 1
+    attempt(x)
+    print(arr)
 
-print("\n Rate:", m/n*100, "%")
+    if len(arr) < x:
+        for j in range(x-len(arr)):
+            arr.append("")
+
+    data.append(arr)
+    arr = []
+
+with open("collection.csv", mode="w", newline="") as f:
+    w = writer(f)
+    w.writerows(data)
